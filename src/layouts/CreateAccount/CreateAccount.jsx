26fcaa82018487eeb9828e0df8962d2400ff7c8a
Shreedutt <shreedutt77@gmail.com>
2022-05-12 23:37:15 +0530

    Container is added in create account page

--- src/layouts/CreateAccount/CreateAccount.jsx
+++ src/layouts/CreateAccount/CreateAccount.jsx
@@ -5,21 +5,30 @@ import GraphImg from '../../assets/images/background.png'
 function CreateAccount() {
     return (
         <div className='createAccount__layout'>
-            <div className="createAccount__layout--left">
-                <div className="image__container">
-                    <img src={GraphImg} alt="graph" />
-                </div>
-                <div className="info">
-                    <div className="info__title">
-                        Choose a date range
+            <div className="layout__container">
+                <div className="createAccount__layout--left">
+                    <div className="image__container">
+                        <img src={GraphImg} alt="graph" />
                     </div>
-                    <div className="info__para">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, consequatur tenetur.
+                    <div className="info">
+                        <div className="info__title">
+                            Choose a date range
+                        </div>
+                        <div className="info__para">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, consequatur tenetur.
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="createAccount__layout--right">
+                <div className="createAccount__layout--right">
+                    <div className="form__container">
+                        <div className="form__title">
+                            Create an account
+                        </div>
+                        <form >
 
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     )
