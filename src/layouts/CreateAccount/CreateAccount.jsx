3ceb6433d23c2e6c4444424fd9d515422b43a57e
Shreedutt <shreedutt77@gmail.com>
2022-05-14 14:30:00 +0530

    check input is now controlled

--- src/layouts/CreateAccount/CreateAccount.jsx
+++ src/layouts/CreateAccount/CreateAccount.jsx
@@ -67,20 +67,26 @@ function CreateAccount() {
                             >
                                 <div className="checkbox--wrapper">
                                     <svg className="checkbox--symbol">
-                                        <symbol id="check"
+                                        <symbol
+                                            id="check"
                                             viewBox="0 0 12 10"
                                         >
                                             <polyline
                                                 points="1.5 6 4.5 9 10.5 1"
                                                 strokeLinejoin="round"
                                                 strokeLinecap="round"
-                                                strokeOpacity="2"
+                                                strokeOpacity="1"
                                             ></polyline>
                                         </symbol>
                                     </svg>
                                     <div className="checkbox__container">
-                                        <input className="checkbox-input" id="apples" type="checkbox" />
-                                        <label className="checkbox" htmlFor="apples">
+                                        <input
+                                            className="checkbox__input" id="custom__checkbox"
+                                            type="checkbox"
+                                            value={inputValue}
+                                        />
+                                        <label
+                                            className="checkbox" htmlFor="custom__checkbox">
                                             <span>
                                                 <svg width="12px" height="10px">
                                                     <use href="#check"></use>
