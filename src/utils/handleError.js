e44214a6e68ab6e62af908c5c5382d1c728bf0c2
Shreedutt <shreedutt77@gmail.com>
2022-05-15 19:25:20 +0530

    Password validation is improved

--- src/utils/handleError.js
+++ src/utils/handleError.js
@@ -1,6 +1,8 @@
 export const handleError = (values) => {
 	const errors = {};
 	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+	const passwordRegex =
+		/^(?=(.*[a-z]){3,})(?=(.*[A-Z]){1,})(?=(.*[0-9]){1,})(?=(.*[!@#$%^&*()\-__+.]){1,}).{8,}$/i;
 	const phoneNumberRegex =
 		/^\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*(\d{1,2})$/i;
 	if (!values.name) {
@@ -15,8 +17,11 @@ export const handleError = (values) => {
 		errors.password = "Password is required";
 	} else if (values.password.length < 4) {
 		errors.password = "Password must be more than 8 characters";
-	} else if (values.password.length > 10) {
+	} else if (values.password.length > 15) {
 		errors.password = "Password cannot exceed more than 15 characters";
+	} else if (!passwordRegex.test(values.password)) {
+		errors.password =
+			"Password must contain uppercase letter, lowercase letter, number and special character";
 	}
 	if (!values.confirmPassword) {
 		errors.confirmPassword = "Password Confirmation is required";
